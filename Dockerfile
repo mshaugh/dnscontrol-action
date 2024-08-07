f2bc54618c3859a2ba5028ba25e0a769ea3c9268
Max Shaughnessy <mshaugh@hey.com>
2024-08-07 12:12:28 +0100

    feat: add github action

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM stackexchange/dnscontrol:4.12.5@sha256:6fd8aa6b75ea49c4f7e154e91636762d3df5c395bcee380f9f092657e4cdfc4b
+
+LABEL repository="https://github.com/mshaugh/dnscontrol-action"
+LABEL maintainer="Max Shaughnessy <mshaugh@hey.com>"
+
+LABEL "com.github.actions.name"="DNSControl"
+LABEL "com.github.actions.description"="Maintain your DNS data with DNSControl and GitHub Actions"
+LABEL "com.github.actions.icon"="globe"
+LABEL "com.github.actions.color"="blue"
+
+RUN apk update && apk add --no-cache bash~=5
+
+RUN ["dnscontrol", "version"]
+
+COPY entrypoint.sh /
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+# Resolve files to absolute paths
+CONFIG_ABS_PATH="$(readlink -f "${INPUT_CONFIG_FILE}")"
+CREDS_ABS_PATH="$(readlink -f "${INPUT_CREDS_FILE}")"
+
+WORKING_DIR="$(dirname "${CONFIG_ABS_PATH}")"
+cd "$WORKING_DIR" || exit
+
+ARGS=(
+    "$@"
+    --config "$CONFIG_ABS_PATH"
+)
+
+# `check` subcommand doesn't require credentials
+if [ "$1" != "check" ]; then
+    ARGS+=(--creds "$CREDS_ABS_PATH")
+fi
+
+IFS=
+OUTPUT="$(dnscontrol "${ARGS[@]}")"
+EXIT_CODE="$?"
+
+echo "$OUTPUT"
+
+# Filter output to reduce 'preview' PR comment length
+FILTERED_OUTPUT="$(echo "$OUTPUT" |\
+    grep -v -e '^\.\.\.0 corrections$' |\
+    grep -v -e '^0 corrections' |\
+    grep -v -e '\.\.\. (skipping)' |\
+    grep -v -e '^----- DNS Provider: ' |\
+    grep -v -e '^----- Registrar: ' |\
+    grep -v -e '^----- Getting nameservers from:' |\
+    sed -e 's/\x1b\[[0-9;]*m//g')"
+
+# Set output
+# https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#multiline-strings
+DELIMITER="DNSCONTROL-$RANDOM"
+
+{
+    echo "output<<$DELIMITER"
+    echo "$OUTPUT"
+    echo "$DELIMITER"
+
+    echo "preview_comment<<$DELIMITER"
+    echo "$FILTERED_OUTPUT"
+    echo "$DELIMITER"
+} >> "$GITHUB_OUTPUT"
+
+exit $EXIT_CODE
